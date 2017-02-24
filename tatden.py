f414d3562973490e2b825ba9fdb5405fa7586288
socbay2mee <socbay2me@yahoo.com>
2017-02-24 15:28:36 +0700

    QQ

--- tatden.py
+++
@@ -1,4 +0,0 @@
-import RPi.GPIO as GPIO 
-GPIO.setmode(GPIO.BOARD) 
-GPIO.setup(7, GPIO.OUT) 
-GPIO.output(7,False) 
---
+++ tatden1.php
@@ -0,0 +1,4 @@
+<?php
+	echo" da tat den  ";
+	exec('sudo python /var/www/html/Project/tatden1.py');
+?>
