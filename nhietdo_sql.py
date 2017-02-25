202560141b2011c62c3d21e2e8d8b0eb561fd504
socbay2mee <socbay2me@yahoo.com>
2017-02-25 15:48:01 +0700

    AA

--- nhietdo_sql.py
+++ nhietdo_sql.py
@@ -4,28 +4,12 @@ import MySQLdb
 db = MySQLdb.connect("localhost","root","nightsky","sinhvien" )
 
 # chuan bi mot doi tuong cursor boi su dung phuong thuc cursor()
-cursor = db.cursor()
+#cursor = db.cursor()
 
 # Xoa bang neu no da ton tai boi su dung phuong thuc execute().
-cursor.execute("DROP TABLE IF EXISTS NHIETDO")
+#cursor.execute("DROP TABLE IF EXISTS NHIETDO")
 
 # Tao mot bang
-sql = """CREATE TABLE NHIETDO (
-         CAMBIEN  CHAR(20) NOT NULL,
-		 ND FLOAT,
-         DA FLOAT )"""
-sql = """INSERT INTO SINHVIEN(CAMBIEN,
-         ND,DA)
-         VALUES ('DHT11',27,89)"""
-
-try:
-   # Thuc thi lenh SQL
-   cursor.execute(sql)
-   # Commit cac thay doi vao trong Database
-   db.commit()
-except:
-   # Rollback trong tinh huong co bat ky error nao
-   db.rollback()
-
-# ngat ket noi voi server
-db.close()
+#sql = """CREATE TABLE NHIETDO (CAMBIEN  CHAR(20) NOT NULL,ND FLOAT,DA FLOAT )"""
+sqlCommand = "INSERT INTO NHIETDO SET CAMBIEN='%s', ND='%s', DA='%s'" % ('DHT11','11','70')
+databaseHelper(sqlCommand,"Insert")
