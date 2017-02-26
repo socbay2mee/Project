d05a86f8eb6068e0590d291eb42c0400a22369d7
socbay2mee <socbay2me@yahoo.com>
2017-02-26 09:49:11 +0700

    qq

--- index.php
+++ index.php
@@ -83,54 +83,50 @@
 		</div>	
 		<div class ="thietbi" >		 
 			<img src="https://image.flaticon.com/icons/svg/214/214326.svg" style="width:50px" onclick="getnhietdo();">
-			<p id="demo3"></p>
-			
+			<p id="demo3"></p>			
 			<?php
-$dbhost = 'localhost:3306';
-$dbuser = 'root';
-$dbpass = 'nightsky';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-if(! $conn )
-{
-  die('Khong the ket noi: ' . mysql_error());
-}
-$sql = 'SELECT TRANGTHAI
-        FROM DEVICE
-		WHERE THIETBI="NHIETDO"';
+				$dbhost = 'localhost:3306';
+				$dbuser = 'root';
+				$dbpass = 'nightsky';
+				$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+				if(! $conn )
+				{
+					die('Khong the ket noi: ' . mysql_error());
+				}
+				$sql = 'SELECT TRANGTHAI
+						FROM DEVICE
+						WHERE THIETBI="NHIETDO"';
 
-mysql_select_db('sinhvien');
-$retval = mysql_query( $sql, $conn );
-if(! $retval )
-{
-  die('Khong the lay du lieu: ' . mysql_error());
-}
-while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
-{
-    echo "nhietdo :{$row['TRANGTHAI']}  <br> ".
-        //"doam: {$row['TRANGTHAI']} <br> ".
-         "--------------------------------<br>";
-} 
-
-$sql = 'SELECT TRANGTHAI
-        FROM DEVICE
-		WHERE THIETBI="DOAM"';
-
-mysql_select_db('sinhvien');
-$retval = mysql_query( $sql, $conn );
-if(! $retval )
-{
-  die('Khong the lay du lieu: ' . mysql_error());
-}
-while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
-{
-    echo //"nhietdo :{$row['TRANGTHAI']}  <br> ".
-         "doam: {$row['TRANGTHAI']} <br> ".
-         "--------------------------------<br>";
-} 
-echo "Lay du lieu thanh cong\n";
-mysql_close($conn);
-?>
-
+				mysql_select_db('sinhvien');
+				$retval = mysql_query( $sql, $conn );
+				if(! $retval )
+				{
+					die('Khong the lay du lieu: ' . mysql_error());
+				}
+				while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+				{
+					echo "nhietdo :{$row['TRANGTHAI']}  <br> ".
+					//"doam: {$row['TRANGTHAI']} <br> ".
+					"--------------------------------<br>";
+				} 
+				$sql = 'SELECT TRANGTHAI
+						FROM DEVICE
+						WHERE THIETBI="DOAM"';
+				mysql_select_db('sinhvien');
+				$retval = mysql_query( $sql, $conn );
+				if(! $retval )
+				{
+					die('Khong the lay du lieu: ' . mysql_error());
+				}
+				while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+				{
+					echo //"nhietdo :{$row['TRANGTHAI']}  <br> ".
+					"doam: {$row['TRANGTHAI']} <br> ".
+					"--------------------------------<br>";
+				} 
+				echo "Lay du lieu thanh cong\n";
+				mysql_close($conn);
+			?>
 		</div>
 	</body>
 </html>
