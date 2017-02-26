f7ed1a77981369e7f5889c454922b4497ed751b5
socbay2mee <socbay2me@yahoo.com>
2017-02-26 09:27:28 +0700

    qqqq

---
+++ GET_PI_PHP.php
@@ -0,0 +1,29 @@
+<?php
+$dbhost = 'localhost:3306';
+$dbuser = 'root';
+$dbpass = 'nightsky';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+  die('Khong the ket noi: ' . mysql_error());
+}
+$sql = 'SELECT THIETBI, 
+              TRANGTHAI
+        FROM DEVICE
+		WHERE THIETBI="NHIETDO"';
+
+mysql_select_db('sinhvien');
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Khong the lay du lieu: ' . mysql_error());
+}
+while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+{
+    echo "nhietdo :{$row['TRANGTHAI']}  <br> ".
+         "doam: {$row['TRANGTHAI']} <br> ".
+         "--------------------------------<br>";
+} 
+echo "Lay du lieu thanh cong\n";
+mysql_close($conn);
+?>
