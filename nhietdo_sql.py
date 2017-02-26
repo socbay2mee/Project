ed224bfb2add19cadf38f6198df70dd355416ebd
socbay2mee <socbay2me@yahoo.com>
2017-02-26 09:27:00 +0700

    qq

--- nhietdo_sql.py
+++ nhietdo_sql.py
@@ -1,15 +1,55 @@
 import MySQLdb
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+import time
 
-# mo ket noi toi Database
+# Adafruit_DHT ho tro nhieu loai cam bien DHT, o day dung DHT11 nen chon cam bien  DHT11
+chon_cam_bien = Adafruit_DHT.DHT11
+GPIO.setmode(GPIO.BCM)
+pin_sensor = 14
+
+print ("RASPI.VN Demo cam bien do am DHT 11");
+do_am, nhiet_do = Adafruit_DHT.read_retry(chon_cam_bien, pin_sensor);
+
+# Open database connection
 db = MySQLdb.connect("localhost","root","nightsky","sinhvien" )
 
-# chuan bi mot doi tuong cursor boi su dung phuong thuc cursor()
-#cursor = db.cursor()
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+# Drop table if it already exist using execute() method.
+cursor.execute("DROP TABLE IF EXISTS DEVICE")
+# Create table as per requirement
+sql = """CREATE TABLE DEVICE (
+         THIETBI  CHAR(20) NOT NULL,
+         TRANGTHAI FLOAT(4,2))"""
 
-# Xoa bang neu no da ton tai boi su dung phuong thuc execute().
-#cursor.execute("DROP TABLE IF EXISTS NHIETDO")
-
-# Tao mot bang
-#sql = """CREATE TABLE NHIETDO (CAMBIEN  CHAR(20) NOT NULL,ND FLOAT,DA FLOAT )"""
-sqlCommand = "INSERT INTO NHIETDO SET CAMBIEN='%s', ND='%s', DA='%s'" % ('DHT11','11','70')
-databaseHelper(sqlCommand,"Insert")
+cursor.execute(sql)
+sql = "INSERT INTO DEVICE(THIETBI, \
+       TRANGTHAI) \
+       VALUES ('%s', '%f')" % \
+       ('DEN',0)
+cursor.execute(sql)
+sql = "INSERT INTO DEVICE(THIETBI, \
+       TRANGTHAI) \
+       VALUES ('%s', '%f')" % \
+       ('QUAT',0)
+cursor.execute(sql)
+sql = "INSERT INTO DEVICE(THIETBI, \
+       TRANGTHAI) \
+       VALUES ('%s', '%f')" % \
+       ('NHIETDO',nhiet_do)
+cursor.execute(sql)
+sql = "INSERT INTO DEVICE(THIETBI, \
+       TRANGTHAI) \
+       VALUES ('%s', '%f')" % \
+       ('DOAM',do_am)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+# disconnect from server
+db.close()
