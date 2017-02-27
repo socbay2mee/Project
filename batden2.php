dba498ccc17dbdd1d7e3fe21fa3e98616862d745
socbay2mee <socbay2me@yahoo.com>
2017-02-27 22:22:52 +0700

    wwww

--- batden2.php
+++
@@ -1,4 +0,0 @@
-<?php
-	echo" da bat den ";
-	exec('sudo python /var/www/html/Project/batden2.py');
-?>
--- batden2.py
+++
@@ -1,4 +0,0 @@
-import RPi.GPIO as GPIO 
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(3, GPIO.OUT) 
-GPIO.output(3,GPIO.HIGH) 
