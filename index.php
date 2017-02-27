af4dc37b94c6404c0b0b590d4058314dedcd45cd
socbay2mee <socbay2me@yahoo.com>
2017-02-27 23:29:38 +0700

    aaa

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
 			}			
 		</script>
    </head>
-   <body>
+   <body onmousemove="getnhietdo();">
 		<div>
 			<img id ="logospkt" src="icon/spkt.png" alt="hcmute.edu.vn" width="590" height="135">
 		</div>  <br/>	
