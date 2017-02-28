07b7049ac4f418b8cdad3ffd74870b0f539bc978
socbay2mee <socbay2me@yahoo.com>
2017-02-28 22:38:10 +0700

    q

--- sql/post_data_to_pi.py
+++ sql/post_data_to_pi.py
@@ -3,6 +3,16 @@ import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
 
+import webbrowser
+import os, subprocess
+import re, serial
+import smtplib
+from email import Encoders
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+
+
 # Adafruit_DHT ho tro nhieu loai cam bien DHT, o day dung DHT11 nen chon cam bien  DHT11
 chon_cam_bien = Adafruit_DHT.DHT11
 GPIO.setmode(GPIO.BCM)
@@ -52,3 +62,34 @@ except:
    db.rollback()
 # disconnect from server
 db.close()
+
+# Ham gui email
+def send_email():    
+    UserName = "socbay2mee@gmail.com"
+    Password = "socbay2me"
+    Recipient = "thuanle.ute@gmail.com"
+    
+    msg = MIMEMultipart()
+    msg['From'] = UserName
+    msg['To'] = Recipient
+    msg['Subject'] = "High temperature detected on " 
+    text = "The house may be burning now. Temperature is: "
+    msg.attach( MIMEText(text) ) 
+    
+    #part = MIMEBase("application", "octet-stream")
+    #fo=open(file,"rb")
+    #part.set_payload(fo.read() )
+    #Encoders.encode_base64(part)
+    #part.add_header('Content-Disposition', 'attachment; filename="%s"'  %os.path.basename(file))
+    #msg.attach(part)
+ 
+    s = smtplib.SMTP('smtp.gmail.com:587')
+    s.ehlo()
+    s.starttls()
+    s.login(UserName, Password)
+    s.sendmail(UserName, Recipient, msg.as_string())
+    s.close()
+
+ if nhiet_do > 30:
+	send_email()
+	
