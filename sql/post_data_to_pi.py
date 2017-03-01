3ddb32fb9d83e234acd3795f963f2bf7ccf72030
socbay2mee <socbay2me@yahoo.com>
2017-03-01 22:59:00 +0700

    sdfsd

--- sql/post_data_to_pi.py
+++ sql/post_data_to_pi.py
@@ -18,8 +18,8 @@ from email.MIMEText import MIMEText
 chon_cam_bien = Adafruit_DHT.DHT11
 GPIO.setmode(GPIO.BCM)
 pin_sensor = 14
-GPIO.setup(2, GPIO.IN)
-GPIO.setup(3, GPIO.IN)
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
 
 den_1 = GPIO.input(2)
 den_2 = GPIO.input(3)
