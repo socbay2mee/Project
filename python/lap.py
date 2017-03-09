45f2ba83930cbbd4d998746276d32f07b1ba83e1
socbay2mee <socbay2me@yahoo.com>
2017-03-10 00:12:04 +0700

    fdsf

--- python/lap.py
+++ python/lap.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO 
-from time import sleep
+import time
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(21, GPIO.OUT)
 while(1):
 	GPIO.output(21,GPIO.HIGH) 
-	sleep(10)
+	time.sleep(2)
 	GPIO.output(21,GPIO.LOW) 
 	
