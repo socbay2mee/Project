ece34d8eb1f48578a63c7250bf671dfe53f4a922
socbay2mee <socbay2me@yahoo.com>
2017-03-10 09:57:53 +0700

    dsd

--- python/batden1.py
+++ python/batden1.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+import smbus
+from time import sleep
+bus = smbus.SMBus(1)
+addr = 0x08
 import RPi.GPIO as GPIO 
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(2, GPIO.OUT) 
-GPIO.output(2,GPIO.HIGH) 
+GPIO.setup(23, GPIO.OUT) 
+GPIO.output(23,GPIO.HIGH)    
+bus.write_byte(addr, 1)
+print(1)
+sleep(1)
--- python/batden2.py
+++ python/batden2.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+import smbus
+from time import sleep
+bus = smbus.SMBus(1)
+addr = 0x08
 import RPi.GPIO as GPIO 
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(3, GPIO.OUT)
-GPIO.output(3,GPIO.HIGH) 
+GPIO.setup(24, GPIO.OUT) 
+GPIO.output(24,GPIO.HIGH)    
+bus.write_byte(addr, 2)
+print(2)
+sleep(1)
