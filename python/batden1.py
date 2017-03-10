4d4d2e41716ead85b1002558682d1e0e0b200c85
socbay2mee <socbay2me@yahoo.com>
2017-03-10 10:18:40 +0700

    dasd

--- python/batden1.py
+++ python/batden1.py
@@ -7,8 +7,7 @@ val = 0x10
 import RPi.GPIO as GPIO 
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(23, GPIO.OUT) 
-GPIO.output(23,GPIO.HIGH) 
-while True:    
-    bus.write_byte(addr, val)
-    print(val)
-    sleep(1)
+GPIO.output(23,GPIO.HIGH)     
+bus.write_byte(addr, val)
+print(val)
+sleep(1)
